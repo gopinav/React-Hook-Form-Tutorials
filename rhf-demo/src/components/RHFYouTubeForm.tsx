badab89a65151055b95302bd730abdcf6bfaa30e
Vishwas <vishwas.g5@gmail.com>
2023-03-11 19:50:17 +0530

    Form submission

--- rhf-demo/src/components/RHFYouTubeForm.tsx
+++ rhf-demo/src/components/RHFYouTubeForm.tsx
@@ -3,15 +3,26 @@ import { DevTool } from "@hookform/devtools";
 
 let count = 0;
 
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
 export const RHFYouTubeForm = () => {
-  const form = useForm();
-  const { register, control } = form;
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+
+  const onSubmit = (data: FormValues) => {
+    console.log("Form submitted", data);
+  };
+
   count++;
   return (
     <div>
       <h1>YouTube Form ({count})</h1>
 
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
 

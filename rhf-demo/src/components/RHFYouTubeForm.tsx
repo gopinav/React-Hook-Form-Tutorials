6fe5a701d24183c432e06a221603e6723c517228
Vishwas <vishwas.g5@gmail.com>
2023-03-11 13:48:10 +0530

    Register fields

--- rhf-demo/src/components/RHFYouTubeForm.tsx
+++ rhf-demo/src/components/RHFYouTubeForm.tsx
@@ -2,19 +2,20 @@ import { useForm } from "react-hook-form";
 
 export const RHFYouTubeForm = () => {
   const form = useForm();
+  const { register } = form;
   return (
     <div>
       <h1>YouTube Form</h1>
 
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username" {...register("username")} />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
       </form>

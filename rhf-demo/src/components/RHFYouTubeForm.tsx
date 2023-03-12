099cc91390b5d3ce4cc29500b1014c0e37640bbe
Vishwas <vishwas.g5@gmail.com>
2023-03-12 14:21:19 +0530

    Custom validation

--- rhf-demo/src/components/RHFYouTubeForm.tsx
+++ rhf-demo/src/components/RHFYouTubeForm.tsx
@@ -44,6 +44,20 @@ export const RHFYouTubeForm = () => {
                 /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
               message: "Invalid email format",
             },
+            validate: {
+              notAdmin: (fieldValue) => {
+                return (
+                  fieldValue !== "admin@example.com" ||
+                  "Enter a different email address"
+                );
+              },
+              notBlackListed: (fieldValue) => {
+                return (
+                  !fieldValue.endsWith("baddomain.com") ||
+                  "This domain is not supported"
+                );
+              },
+            },
           })}
         />
         <p className="error">{errors.email?.message}</p>

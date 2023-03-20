fa2ac48460d954828e4c6e4a8c2fe8e2749ee0c3
Vishwas <vishwas.g5@gmail.com>
2023-03-20 09:33:53 +0530

    Refactor CSS

--- rhf-demo/src/components/RHFYouTubeForm.tsx
+++ rhf-demo/src/components/RHFYouTubeForm.tsx
@@ -103,103 +103,117 @@ export const RHFYouTubeForm = () => {
 
       {/* <h2>Watched value: {watchUsername}</h2> */}
       <form onSubmit={handleSubmit(onSubmit, onError)}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          {...register("username", {
-            required: { value: true, message: "Username is required" },
-          })}
-        />
-        <p className="error">{errors.username?.message}</p>
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: { value: true, message: "Username is required" },
+            })}
+          />
+          <p className="error">{errors.username?.message}</p>
+        </div>
 
-        <label htmlFor="email">E-mail</label>
-        <input
-          type="email"
-          id="email"
-          {...register("email", {
-            pattern: {
-              value:
-                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              message: "Invalid email format",
-            },
-            validate: {
-              notAdmin: (fieldValue) => {
-                return (
-                  fieldValue !== "admin@example.com" ||
-                  "Enter a different email address"
-                );
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
               },
-              notBlackListed: (fieldValue) => {
-                return (
-                  !fieldValue.endsWith("baddomain.com") ||
-                  "This domain is not supported"
-                );
+              validate: {
+                notAdmin: (fieldValue) => {
+                  return (
+                    fieldValue !== "admin@example.com" ||
+                    "Enter a different email address"
+                  );
+                },
+                notBlackListed: (fieldValue) => {
+                  return (
+                    !fieldValue.endsWith("baddomain.com") ||
+                    "This domain is not supported"
+                  );
+                },
+                emailAvailable: async (fieldValue) => {
+                  const response = await fetch(
+                    `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
+                  );
+                  const data = await response.json();
+                  return data.length === 0 || "Email already exists";
+                },
               },
-              emailAvailable: async (fieldValue) => {
-                const response = await fetch(
-                  `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
-                );
-                const data = await response.json();
-                return data.length === 0 || "Email already exists";
-              },
-            },
-          })}
-        />
-        <p className="error">{errors.email?.message}</p>
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
 
-        <label htmlFor="channel">Channel</label>
-        <input
-          type="text"
-          id="channel"
-          {...register("channel", {
-            required: { value: true, message: "Channel is required" },
-          })}
-        />
-        <p className="error">{errors.channel?.message}</p>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", {
+              required: { value: true, message: "Channel is required" },
+            })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
 
-        <label htmlFor="address-line1">Address Line 1</label>
-        <input
-          type="text"
-          id="address-line1"
-          {...register("address.line1", {
-            required: { value: true, message: "Address is required" },
-          })}
-        />
-        <p className="error">{errors.address?.line1?.message}</p>
+        <div className="form-control">
+          <label htmlFor="address-line1">Address Line 1</label>
+          <input
+            type="text"
+            id="address-line1"
+            {...register("address.line1", {
+              required: { value: true, message: "Address is required" },
+            })}
+          />
+          <p className="error">{errors.address?.line1?.message}</p>
+        </div>
 
-        <label htmlFor="address-line2">Address Line 2</label>
-        <input
-          type="text"
-          id="address-line2"
-          {...register("address.line2", {
-            required: { value: true, message: "Address is required" },
-            disabled: watch("address.line1") === "",
-          })}
-        />
-        <p className="error">{errors.address?.line2?.message}</p>
+        <div className="form-control">
+          <label htmlFor="address-line2">Address Line 2</label>
+          <input
+            type="text"
+            id="address-line2"
+            {...register("address.line2", {
+              required: { value: true, message: "Address is required" },
+              disabled: watch("address.line1") === "",
+            })}
+          />
+          <p className="error">{errors.address?.line2?.message}</p>
+        </div>
 
-        <label htmlFor="age">Age</label>
-        <input
-          type="number"
-          id="age"
-          {...register("age", {
-            valueAsNumber: true,
-            required: { value: true, message: "Age is required" },
-          })}
-        />
-        <p className="error">{errors.age?.message}</p>
+        <div className="form-control">
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            {...register("age", {
+              valueAsNumber: true,
+              required: { value: true, message: "Age is required" },
+            })}
+          />
+          <p className="error">{errors.age?.message}</p>
+        </div>
 
-        <label htmlFor="dob">Date of Birth</label>
-        <input
-          type="date"
-          id="dob"
-          {...register("dob", {
-            valueAsDate: true,
-            required: { value: true, message: "Date of Birth is required" },
-          })}
-        />
-        <p className="error">{errors.dob?.message}</p>
+        <div className="form-control">
+          <label htmlFor="dob">Date of Birth</label>
+          <input
+            type="date"
+            id="dob"
+            {...register("dob", {
+              valueAsDate: true,
+              required: { value: true, message: "Date of Birth is required" },
+            })}
+          />
+          <p className="error">{errors.dob?.message}</p>
+        </div>
 
         <button type="button" onClick={handleGetValues}>
           Get values
